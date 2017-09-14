69b67417364d0b350b5e16227f2211b0b05871fb
Kugak <kugarajk@sheridanc.on.ca>
2017-09-14 12:49:04 -0400

    Initial Commit

---
+++ w2SpecialCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  w2SpecialCalculator
+//
+//  Created by Kuga Kugaraja on 2017-09-14.
+//  Copyright © 2017 Kuga Kugaraja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
